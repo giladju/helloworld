0ea6ddf845a29fb338da11aa52b8776f2acc7bd2
Gilad Judes <gilad@tikalk.com>
2014-07-09 10:10:31 +0300

    updated build.gradle and added settings.gradle to support multi-project build

--- api/src/main/java/org/gradle/example/api/HelloWorldapi.java
+++ api/src/main/java/org/gradle/example/api/HelloWorldapi.java
@@ -1,6 +1,6 @@
 package org.gradle.example.api;
 
-public class HelloWorld-api {
+public class HelloWorldapi {
    public static void main(String args[]) {
      System.out.println("Hello, Universe");
    }
